8e2561b83b701f9d1bb743c07a16c2ebb82ecdda
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-15 23:50:07 +0300

    app-routing module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'sayfa/:page', // :page => page kısmına gelen değeri algılayabiliyor olacak.
+        component: HomeComponent
+      },
+      {
+        path: 'hakkimda',
+        component: AboutMeComponent,
+      },
+      {
+        path: 'iletisim',
+        component: ContactComponent,
+      }
+    ]
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent
+  }
+];
+
+// www.bidibidi.com/
+// www.bidibidi.com/hakkimizda
+// www.bidibidi.com/admin
+// www.bidibidi.com/admin/article/add
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

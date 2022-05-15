821c3a872d370ed79705216a5af71b975cedbd7c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-15 23:49:23 +0300

    app module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MainModule } from './pages/main.module';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 import { AppComponent } from './app.component';
+
+
+import { AdminNavComponent } from './nav/admin-nav/admin-nav.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLayoutComponent,
+    AdminNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MainModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], // Uygulamanın ilk çalıştığı zaman hangi component ile başlıyacağını belirtiyoruz.
 })
-export class AppModule { }
+export class AppModule {}
+
+// Mümkün olduğunca burayı temiz tutmalıyız.
